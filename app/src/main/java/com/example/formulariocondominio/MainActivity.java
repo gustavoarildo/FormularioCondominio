fb3ef7f77248b42123ab6b2c091cf50f3f0e4759
gustavoarildo <gustavoarildo@gmail.com>
2020-05-16 15:23:51 -0300

    #4 menu uperior esta funcional

--- app/src/main/java/com/example/formulariocondominio/MainActivity.java
+++ app/src/main/java/com/example/formulariocondominio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.formulariocondominio;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,8 +12,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<ResponsavelFinanceiro> listResponsaveisFinanceiros = new ArrayList<ResponsavelFinanceiro>();
+
+    EditText edtID;
+    EditText edtNOME;
+    EditText edtTELEFONE;
+    EditText edtMENSALIDADE;
+    EditText edtDEBITOTOTAL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        edtID = (EditText)findViewById(R.id.editText_Identificacao);
+        edtNOME = (EditText)findViewById(R.id.editText_Nome);
+        edtTELEFONE = (EditText)findViewById(R.id.editText_Telefone);
+        edtMENSALIDADE = (EditText)findViewById(R.id.editText_Mensalidade);
+        edtDEBITOTOTAL = (EditText)findViewById(R.id.editText_DebitoTotal);
     }
 
     @Override
@@ -47,10 +66,92 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_menu_consulta_codigo) {
+
+            edtID = (EditText)findViewById(R.id.editText_Identificacao);
+            edtNOME = (EditText)findViewById(R.id.editText_Nome);
+            edtTELEFONE = (EditText)findViewById(R.id.editText_Telefone);
+            edtMENSALIDADE = (EditText)findViewById(R.id.editText_Mensalidade);
+            edtDEBITOTOTAL = (EditText)findViewById(R.id.editText_DebitoTotal);
+
+            try {
+                boolean achei = false;
+                boolean idlistado = false;
+                for (int i = 0; i < listResponsaveisFinanceiros.size(); i++) {
+                    ResponsavelFinanceiro rf = (ResponsavelFinanceiro) listResponsaveisFinanceiros.get(i);
+                    if (rf.getId() == (Integer.parseInt(edtID.getText().toString()))  ) {
+                        achei = true;
+                    }
+
+                    if (achei) {
+                        edtNOME.setText(rf.getNome());
+                        edtTELEFONE.setText(String.valueOf(rf.getTelefone()));
+                        edtMENSALIDADE.setText( String.valueOf(rf.getValorMensalidade()));
+                        edtDEBITOTOTAL.setText( String.valueOf(rf.getDebitoTotal()));
+
+                        achei = false;
+                        idlistado = true;
+                    }
+                } // for
+                if(listResponsaveisFinanceiros.size()>0 && idlistado == true) {
+                    exibeTextoNaTela("CONSULTA POR CODIGO BEM SUCEDIDA, DADOS EXIBIDOS NOS CAMPOS");
+                }
+                else{
+                    exibeTextoNaTela("LISTA NÃO POSSUI ESSE ID");
+                }
+            }
+            catch (Exception e){
+                exibeTextoNaTela("CONSULTA POR CODIGO MAL SUCEDIDA, PREENCHA O CAMPO ID COM O ID DO ITEM PROCURADO");
+            }
+
+
+
+
+
             return true;
         }
 
         if (id == R.id.action_menu_cadastrar) {
+            edtID = (EditText)findViewById(R.id.editText_Identificacao);
+            edtNOME = (EditText)findViewById(R.id.editText_Nome);
+            edtTELEFONE = (EditText)findViewById(R.id.editText_Telefone);
+            edtMENSALIDADE = (EditText)findViewById(R.id.editText_Mensalidade);
+            edtDEBITOTOTAL = (EditText)findViewById(R.id.editText_DebitoTotal);
+
+
+            try {
+                ResponsavelFinanceiro novo = new ResponsavelFinanceiro(
+                        Integer.parseInt(edtID.getText().toString()),
+                        edtNOME.getText().toString(),
+                        Integer.parseInt(edtTELEFONE.getText().toString()),
+                        Double.parseDouble(edtMENSALIDADE.getText().toString()),
+                        Double.parseDouble(edtDEBITOTOTAL.getText().toString())
+                );
+
+                boolean achei = false;
+                for (int i = 0; i < listResponsaveisFinanceiros.size(); i++) {
+                    ResponsavelFinanceiro rf = (ResponsavelFinanceiro) listResponsaveisFinanceiros.get(i);
+                    if (rf.getId() == (Integer.parseInt(edtID.getText().toString()))) {
+                        achei = true;
+                    }
+                }
+
+                if (achei) {
+                    exibeTextoNaTela("CADASTRAMENTO MAL SUCEDIDO, ID JA EM USO NO MOMENTO");
+
+                }
+                else{
+                    listResponsaveisFinanceiros.add(novo);
+                    exibeTextoNaTela("CADASTRAMENTO BEM SUCEDIDO");
+                    achei = false;
+                }
+            }
+            catch (Exception e){
+                exibeTextoNaTela("CADASTRAMENTO MAL SUCEDIDO PREENCHER TODOS CAMPOS");
+            }
+
+
+
+
             return true;
         }
 
@@ -59,13 +160,107 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (id == R.id.action_menu_exclusao){
+
+            edtID = (EditText)findViewById(R.id.editText_Identificacao);
+            edtNOME = (EditText)findViewById(R.id.editText_Nome);
+            edtTELEFONE = (EditText)findViewById(R.id.editText_Telefone);
+            edtMENSALIDADE = (EditText)findViewById(R.id.editText_Mensalidade);
+            edtDEBITOTOTAL = (EditText)findViewById(R.id.editText_DebitoTotal);
+
+
+            try {
+
+                boolean achei = false;
+                for (int i = 0; i < listResponsaveisFinanceiros.size(); i++) {
+                    ResponsavelFinanceiro rf = (ResponsavelFinanceiro) listResponsaveisFinanceiros.get(i);
+                    if (rf.getId() == (Integer.parseInt(edtID.getText().toString()))  ) {
+                        achei = true;
+                    }
+
+                    if (achei) {
+                        //rf
+                        listResponsaveisFinanceiros.remove(i);
+                        exibeTextoNaTela("EXCLUSÃO POR CODIGO BEM SUCEDIDA, DADOS DELETADOS");
+
+                        achei = false;
+                    }
+                } // for
+
+            }
+            catch (Exception e){
+                exibeTextoNaTela("EXCLUSÃO POR CODIGO MAL SUCEDIDA, PREENCHA O CAMPO ID COM O ID DO ITEM PROCURADO");
+            }
+
+
+
+
             return true;
         }
 
         if (id == R.id.action_menu_alterar){
+
+            edtID = (EditText)findViewById(R.id.editText_Identificacao);
+            edtNOME = (EditText)findViewById(R.id.editText_Nome);
+            edtTELEFONE = (EditText)findViewById(R.id.editText_Telefone);
+            edtMENSALIDADE = (EditText)findViewById(R.id.editText_Mensalidade);
+            edtDEBITOTOTAL = (EditText)findViewById(R.id.editText_DebitoTotal);
+
+            try {
+
+                boolean achei = false;
+                for (int i = 0; i < listResponsaveisFinanceiros.size(); i++) {
+                    ResponsavelFinanceiro rf = (ResponsavelFinanceiro) listResponsaveisFinanceiros.get(i);
+                    if (rf.getId() == (Integer.parseInt(edtID.getText().toString()))  ) {
+                        achei = true;
+                    }
+
+                    if (achei) {
+                        //rf
+                        ResponsavelFinanceiro novo = new ResponsavelFinanceiro(
+                                rf.getId(),
+                                edtNOME.getText().toString(),
+                                Integer.parseInt(edtTELEFONE.getText().toString()),
+                                Double.parseDouble(edtMENSALIDADE.getText().toString()),
+                                Double.parseDouble(edtDEBITOTOTAL.getText().toString())
+                        );
+
+
+
+
+                        listResponsaveisFinanceiros.set(i, novo);
+                        exibeTextoNaTela("ALTERACAO POR CODIGO BEM SUCEDIDA, DADOS MODIFICADOS, ID NAO PODE SER MODIFICADO");
+
+                        achei = false;
+                    }
+
+                } // for
+
+            }
+            catch (Exception e){
+                exibeTextoNaTela("ALTERACAO POR CODIGO MAL SUCEDIDA, PREENCHA O CAMPO ID COM O ID DO ITEM PROCURADO");
+            }
+
+
+
+
+
+
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void exibeTextoNaTela(String meuTexto){
+        Context context = getApplicationContext();
+        CharSequence text = meuTexto;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
 }
